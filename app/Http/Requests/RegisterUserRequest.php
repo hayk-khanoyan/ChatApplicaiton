2db0c08cec999b02ab5ed518551f9abb64cd6153
hayk27277 <haykkhfa@gmail.com>
2023-02-26 00:13:57 +0400

    changes and refactoring auth process

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:6',
+            'password_confirmation' => 'required|string|same:password',
+        ];
+    }
+}
